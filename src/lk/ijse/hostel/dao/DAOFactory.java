91010e1f8dada4dfaa3e0119189496337f074729
Milan-sandeepa <glmsandeepa@gmail.com>
2022-06-27 14:56:35 +0530

    DAOFactory refactored UserDAOImpl

--- src/lk/ijse/hostel/dao/DAOFactory.java
+++ src/lk/ijse/hostel/dao/DAOFactory.java
@@ -1,5 +1,39 @@
 package lk.ijse.hostel.dao;
 
-public class DAOFactory {
+import lk.ijse.hostel.dao.custom.impl.UserDAOImpl;
 
+public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+    }
+
+    public static DAOFactory getDAOFactory() {
+        if (daoFactory == null) {
+            daoFactory = new DAOFactory();
+        }
+        return daoFactory;
+    }
+
+    //factory method
+    public SuperDAO getDAO(DAOTypes types) {
+        switch (types) {
+            case USER:
+                return new UserDAOImpl();
+            case ITEM:
+                return null;
+            case ORDER:
+                return null;
+            case ORDERDETAIL:
+                return null;
+            case QUERYDAO:
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    public enum DAOTypes {
+        USER, ITEM, ORDER, ORDERDETAIL, QUERYDAO
+    }
 }
